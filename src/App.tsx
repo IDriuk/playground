a96b67a99b654e4c77d5916f1f4f6b70abdc2058
Ivan Driuk <veytarn@gmail.com>
2025-03-29 00:43:32 +0200

    styling with utility classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import logo from './img/logo.svg'
 import "./App.css";
 
 function App() {
 
   return (
     <>
-      <div className=" bg-cyan-300"> hello world </div>
+      <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white m-2 p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        <img className="size-12 shrink-0" src={logo} alt="ChitChat Logo" />
+        <div>
+          <div className="text-xl font-medium text-black dark:text-white">ChitChat</div>
+          <p className="text-gray-500 dark:text-gray-400">You have a new message!</p>
+        </div>
+      </div>
     </>
   );
 }
