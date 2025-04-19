b5393dc6d3fb894fc1b0a1072092363450270277
Ivan Driuk <veytarn@gmail.com>
2025-04-19 12:59:25 +0300

    add interesting

---
+++ src/5_interesting/directions_reduction.test.ts
@@ -0,0 +1,12 @@
+// Directions Reduction
+// https://www.codewars.com/kata/550f22f4d758534c1100025a/train/javascript
+// connect: interesting
+
+
+test("reduce opposite directions", () => {
+  // expect(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])).toEqual(["WEST"])
+  // expect(dirReduc(["NORTH", "WEST", "SOUTH", "EAST"])).toEqual(["NORTH", "WEST", "SOUTH", "EAST"])
+  // expect(dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"])).toEqual([])
+
+  expect(1).toBe(1)
+})
