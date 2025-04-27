413511567fb035f0b7969f6946b45949fef6aac9
Ivan Driuk <veytarn@gmail.com>
2025-04-27 16:22:02 +0300

    add matrix

--- src/1_simple/matrix.test.ts
+++ src/1_simple/matrix.test.ts
@@ -14,9 +14,10 @@
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
+// connect: interesting
 
-function matrix(n) {
-  const results = [];
+function matrix(n: number) {
+  const results: number[][] = [];
 
   for (let i = 0; i < n; i++) {
     results.push([]);
@@ -60,4 +61,31 @@ function matrix(n) {
   return results;
 }
 
-module.exports = matrix;
+
+test('matrix is a function', () => {
+  expect(typeof matrix).toEqual('function');
+});
+
+test('matrix produces a 2x2 array', () => {
+  const m = matrix(2);
+  expect(m.length).toEqual(2);
+  expect(m[0]).toEqual([1, 2]);
+  expect(m[1]).toEqual([4, 3]);
+});
+
+test('matrix produces a 3x3 array', () => {
+  const m = matrix(3);
+  expect(m.length).toEqual(3);
+  expect(m[0]).toEqual([1, 2, 3]);
+  expect(m[1]).toEqual([8, 9, 4]);
+  expect(m[2]).toEqual([7, 6, 5]);
+});
+
+test('matrix produces a 4x4 array', () => {
+  const m = matrix(4);
+  expect(m.length).toEqual(4);
+  expect(m[0]).toEqual([1, 2, 3, 4]);
+  expect(m[1]).toEqual([12, 13, 14, 5]);
+  expect(m[2]).toEqual([11, 16, 15, 6]);
+  expect(m[3]).toEqual([10, 9, 8, 7]);
+});
