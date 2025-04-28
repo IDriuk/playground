ae04686e926eaa5d40d08ff3db9f1ebc80aaf735
Ivan Driuk <veytarn@gmail.com>
2025-04-28 12:45:46 +0300

    add palindrome

--- src/1_simple/palindrome.test.ts
+++ src/1_simple/palindrome.test.ts
@@ -1,4 +1,27 @@
-const palindrome = require('./index');
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+// connect: undefined
+
+// function palindrome(str) {
+//   const reversed = str
+//     .split('')
+//     .reverse()
+//     .join('');
+//
+//   return str === reversed;
+// }
+
+function palindrome(str: string) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
+}
 
 test('palindrome function is defined', () => {
   expect(typeof palindrome).toEqual('function');
