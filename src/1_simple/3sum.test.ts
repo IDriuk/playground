656b8bf58154a79da4b77b0684165877da18e0d4
Ivan Driuk <veytarn@gmail.com>
2025-05-02 16:52:32 +0300

    add 3sum

--- src/1_simple/3sum.test.ts
+++ src/1_simple/3sum.test.ts
@@ -1,16 +1,15 @@
 // 3Sum
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
 // https://leetcode.com/problems/3sum/
+// connect: interesting, combinations
 
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
-var threeSum = function(nums) {
+var threeSum = function(nums: number[]) {
   if (nums == null || nums.length < 3) { return [] }
   
   nums.sort( (a, b) => a - b)
   
-  let res = []
+  let res: number[][] = []
  
   for (let i = 0; i < nums.length - 2; i++) {
       if (nums[i] > 0) {
@@ -48,3 +47,9 @@ var threeSum = function(nums) {
   
   return res
 };
+
+test('3Sum', () => {
+  expect(threeSum([-1,0,1,2,-1,-4])).toEqual([[-1,-1,2],[-1,0,1]])
+  expect(threeSum([0,1,1])).toEqual([])
+  expect(threeSum([0,0,0])).toEqual([[0,0,0]])
+})
