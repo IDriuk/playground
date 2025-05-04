69e97930a2ef25bf7992f806fbfca6f42a06cd8f
Ivan Driuk <veytarn@gmail.com>
2025-05-04 16:16:12 +0300

    add best time to buy and sell stock

---
+++ src/5_interesting/best_time_to_buy_and_sell_stock.test.ts
@@ -0,0 +1,11 @@
+// Best Time to Buy and Sell Stock 
+// You are given an array of prices where prices[i] is the price of a given stock on the i-th day.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+// connect: interesting
+
+
+
+test("should return maximum profit for best time transaction", () => {
+  // expect(maxProfit([7,1,5,3,6,4])).toBe(5)
+})
