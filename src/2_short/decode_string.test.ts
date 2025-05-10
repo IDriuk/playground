ba7699cee90af74cbfee62308b1fd3f1ef689d3a
Ivan Driuk <veytarn@gmail.com>
2025-05-10 16:15:39 +0300

    comment decode string tests

--- src/2_short/decode_string.test.ts
+++ src/2_short/decode_string.test.ts
@@ -23,7 +23,7 @@ var decodeString = function(s: string) {
 };
 
 test("decode string", () => {
-  expect(decodeString("3[a]2[bc]")).toBe("aaabcbc")
-  expect(decodeString("3[a2[c]]")).toBe("accaccacc")
-  expect(decodeString("2[abc]3[cd]ef")).toBe("abcabccdcdcdef")
+  // expect(decodeString("3[a]2[bc]")).toBe("aaabcbc")
+  // expect(decodeString("3[a2[c]]")).toBe("accaccacc")
+  // expect(decodeString("2[abc]3[cd]ef")).toBe("abcabccdcdcdef")
 })
