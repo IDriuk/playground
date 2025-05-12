e3194f1a1315e3e611a44c9d4bb8070b431cd39f
Ivan Driuk <veytarn@gmail.com>
2025-05-12 19:01:24 +0300

    add find duplicate number

---
+++ src/5_interesting/find_the_duplicate_number.test.ts
@@ -0,0 +1,12 @@
+// Find the Duplicate Number
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+// There is only one repeated number in nums, return this repeated number.
+// You must solve the problem without modifying the array nums and using only constant extra space.
+// https://leetcode.com/problems/find-the-duplicate-number/
+// connect: interesting, cycle
+
+test("should find duplicate number in array", () => {
+  // expect(findDuplicate([1,3,4,2,2])).toBe(2)
+  // expect(findDuplicate([3,1,3,4,2])).toBe(3)
+  // expect(findDuplicate([3,3,3,3,3])).toBe(3)
+})
