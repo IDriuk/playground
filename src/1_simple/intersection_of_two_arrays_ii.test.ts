aaefe9fc98cd0b1c4c7c913c6f0282ba9dc5b5a5
Ivan Driuk <veytarn@gmail.com>
2025-05-15 13:39:31 +0300

    add intersection of two arrays

--- src/1_simple/intersection_of_two_arrays_ii.test.ts
+++ src/1_simple/intersection_of_two_arrays_ii.test.ts
@@ -1,15 +1,19 @@
 // Intersection of Two Arrays II
+// Given two integer arrays nums1 and nums2, return an array of their intersection. 
+// Each element in the result must appear as many times as it shows in both arrays 
+// and you may return the result in any order.
 // https://leetcode.com/problems/intersection-of-two-arrays-ii/
+// connect: set, interesting
 
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number[]}
  */
-var intersect = function(nums1, nums2) {
+var intersect = function(nums1: number[], nums2: number[]) {
     
   let result = []
-  let hash = {}
+  let hash: Record<number, number> = {}
   
   for ( let i = 0; i < nums1.length; i++) {
       let el = nums1[i]
@@ -30,3 +34,9 @@ var intersect = function(nums1, nums2) {
   
   return result
 };
+
+
+test ("should return intersection", () => {
+  expect(intersect([1,2,2,1], [2,2])).toEqual([2,2])
+  expect(intersect([4,9,5], [9,4,9,8,4])).toEqual([9,4])
+})
