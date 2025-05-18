8369c6b8dda2021a51411127dce15794c462954f
Ivan Driuk <veytarn@gmail.com>
2025-05-18 13:44:43 +0300

    done generate parentheses

--- src/5_interesting/generate_parentheses.test.ts
+++
@@ -1,9 +0,0 @@
-// Generate Parentheses
-// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
-// https://leetcode.com/problems/generate-parentheses/
-// connect: interesting, recursion
-
-test("generate all combinations of parentheses", () => {
-  // expect(generateParenthesis(3)).toEqual(["((()))","(()())","(())()","()(())","()()()"])
-  // expect(generateParenthesis(1)).toEqual(["()"])
-})
