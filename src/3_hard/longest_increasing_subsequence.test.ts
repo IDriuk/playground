1d84580e852f0724865c2f426c16147519f5fe13
Ivan Driuk <veytarn@gmail.com>
2025-05-19 18:22:00 +0300

    add longest increasing subsequence

--- src/3_hard/longest_increasing_subsequence.test.ts
+++ src/3_hard/longest_increasing_subsequence.test.ts
@@ -1,11 +1,9 @@
 // Longest Increasing Subsequence
+// Given an integer array nums, return the length of the longest strictly increasing subsequence.
 // https://leetcode.com/problems/longest-increasing-subsequence/
+// connect: interesting
 
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var lengthOfLIS = function(nums) {
+var lengthOfLIS = function(nums: number[]) {
   if (!nums || nums.length == 0) {
       return 0
   }
@@ -31,3 +29,9 @@ var lengthOfLIS = function(nums) {
   
   return levs.length
 };
+
+test("longest increasing subsequence", () => {
+  expect(lengthOfLIS([10,9,2,5,3,7,101,18])).toBe(4)
+  expect(lengthOfLIS([0,1,0,3,2,3])).toBe(4)
+  expect(lengthOfLIS([7,7,7,7,7,7,7])).toBe(1)
+})
