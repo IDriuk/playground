11f217371e70c2d7b10e6fd2bf74335e76847b28
Ivan Driuk <veytarn@gmail.com>
2025-05-20 18:57:12 +0300

    longest valid parentheses to interesting

---
+++ src/5_interesting/longest_valid_parentheses.test.ts
@@ -0,0 +1,12 @@
+// Longest Valid Parentheses
+// Given a string containing just the characters '(' and ')', 
+// return the length of the longest valid (well-formed) parentheses substring.
+// https://leetcode.com/problems/longest-valid-parentheses/submissions/
+// connect: stack, interesting
+
+
+
+test("longest valid parentheses", () => {
+  // expect(longestValidParentheses("(()")).toBe(2)
+  // expect(longestValidParentheses(")()())")).toBe(4)
+})
