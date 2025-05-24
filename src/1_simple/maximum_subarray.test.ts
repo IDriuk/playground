e0ab5fe03cc094a6980b07c37caaa1a1c2331303
Ivan Driuk <veytarn@gmail.com>
2025-05-25 01:28:12 +0300

    add maximum subarray

--- src/1_simple/maximum_subarray.test.ts
+++ src/1_simple/maximum_subarray.test.ts
@@ -1,11 +1,10 @@
 // Maximum Subarray
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
 // https://leetcode.com/problems/maximum-subarray/submissions/
+// connect: undefinded
 
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var maxSubArray = function(nums) {
+
+var maxSubArray = function(nums: number[]) {
   var maxSum = -Infinity
   var prevSum = 0
   
@@ -26,3 +25,9 @@ var maxSubArray = function(nums) {
   
   return maxSum
 };
+
+test("maximum subarray", () => {
+  expect(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])).toBe(6)
+  expect(maxSubArray([1])).toBe(1)
+  expect(maxSubArray([5,4,-1,7,8])).toBe(23)
+})
