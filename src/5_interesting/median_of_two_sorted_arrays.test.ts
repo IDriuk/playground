b8fa019dbbfbd9ee94de31b3dbb814b9842a2a7d
Ivan Driuk <veytarn@gmail.com>
2025-05-25 15:55:39 +0300

    update median in interesting

--- src/5_interesting/median_of_two_sorted_arrays.test.ts
+++ src/5_interesting/median_of_two_sorted_arrays.test.ts
@@ -4,38 +4,6 @@
 // https://leetcode.com/problems/median-of-two-sorted-arrays/
 // connect: interesting, undefined
 
-var findMedianSortedArrays = function(nums1: number[], nums2: number[]) {
-  let merged = []
-  
-  while ( nums1.length && nums2.length ) {
-      if (nums1[0] < nums2[0]) {
-          merged.push(nums1.shift())
-      } else {
-          merged.push(nums2.shift())
-      }
-  }
-  
-  while ( nums1.length ) {
-      merged.push(nums1.shift())
-  }
-  
-  while ( nums2.length) {
-      merged.push(nums2.shift())
-  }
-  
-  let len = merged.length
-  let median
-  
-  if ( len % 2 ) {
-      median = merged[Math.floor(len / 2)]
-  } else {
-      let left = merged[Math.floor((len - 1) / 2)]!
-      let right = merged[len / 2]!
-      median = (left + right) / 2
-  }
-  
-  return median
-};
 
 test("median of two sorted arrays", () => {
   expect(findMedianSortedArrays([1,3], [2])).toBe(2)
