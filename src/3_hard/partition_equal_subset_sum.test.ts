e629d0c83bd4c8787fd0ce753196126b3b9e9118
Ivan Driuk <veytarn@gmail.com>
2025-05-31 22:43:40 +0300

    add partition equal subset sum

--- src/3_hard/partition_equal_subset_sum.test.ts
+++ src/3_hard/partition_equal_subset_sum.test.ts
@@ -1,11 +1,11 @@
 // Partition Equal Subset Sum
+// Given an integer array nums, return true if you can partition the array into two subsets 
+// such that the sum of the elements in both subsets is equal or false otherwise.
 // https://leetcode.com/problems/partition-equal-subset-sum/submissions/
+// connect: undefined
 
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
-var canPartition = function(nums) {
+
+var canPartition = function(nums: number[]) {
   let total = 0
   for (let num of nums) {
       total += num
@@ -27,3 +27,8 @@ var canPartition = function(nums) {
   
   return tabs[len][subSum]
 };
+
+test("partition equal subset sum", () => {
+  expect(canPartition([1,5,11,5])).toBe(true)
+  expect(canPartition([1,2,3,5])).toBe(false)
+})
