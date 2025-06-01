fc6f1a6019e188bd829324d698d69eca43042b6e
Ivan Driuk <veytarn@gmail.com>
2025-06-01 18:02:48 +0300

    add pascal's triangle

--- src/2_short/pascals_triangle.test.ts
+++ src/2_short/pascals_triangle.test.ts
@@ -1,11 +1,10 @@
 // Pascal's Triangle
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 // https://leetcode.com/problems/pascals-triangle/
+// connect: interesting
 
-/**
- * @param {number} numRows
- * @return {number[][]}
- */
-var generate = function(numRows) {
+var generate = function(numRows: number) {
   if (numRows == 0) return []
   
   let triangle = [[1]]
@@ -27,3 +26,8 @@ var generate = function(numRows) {
   
   return triangle
 };
+
+test("pascal's triangle", () => {
+  expect(generate(5)).toEqual([[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]])
+  expect(generate(1)).toEqual([[1]])
+})
---
+++ src/5_interesting/pascals_triangle.test.ts
@@ -0,0 +1,12 @@
+// Pascal's Triangle
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+// https://leetcode.com/problems/pascals-triangle/
+// connect: interesting
+
+
+
+test("pascal's triangle", () => {
+  // expect(generate(5)).toEqual([[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]])
+  // expect(generate(1)).toEqual([[1]])
+})
